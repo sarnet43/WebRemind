289deaf73d7a45020175538b8750db34c621a1fc
leesua <s2332@e-mirim.hs.kr>
2024-11-11 13:07:12 +0900

    Start 화면 실행

---
+++ src/main/java/com/example/webremind/Startcontroller.java
@@ -0,0 +1,12 @@
+package com.example.webremind;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Startcontroller {
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Text text;
+}
