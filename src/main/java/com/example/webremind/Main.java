6c357800010b06b7252c2cc208a9b83ae77dbca8
leesua <s2332@e-mirim.hs.kr>
2024-11-22 09:45:37 +0900

    시작화면 띄우기

--- src/main/java/com/example/webremind/Main.java
+++ src/main/java/com/example/webremind/Main.java
@@ -1,4 +1,32 @@
 package com.example.webremind;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // FXML 파일 로드
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/startscreen.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Scene 생성 및 Stage 설정
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Web Remind");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/com/example/webremind/MainController.java
@@ -0,0 +1,9 @@
+package com.example.webremind;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+}
