ec12aec8939a043899d9446a4d488a2c4cc9be40
lmslh <s2330@e-mirim.hs.kr>
2024-11-25 10:43:33 +0900

    시작화면, 로그인, 회원가입, 알림 css, fxml 완성

--- src/main/java/com/example/webremind/Main.java
+++ src/main/java/com/example/webremind/Main.java
@@ -1,14 +1,17 @@
 package com.example.webremind;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) {
         try {
