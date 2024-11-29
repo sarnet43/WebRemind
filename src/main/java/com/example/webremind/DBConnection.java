81f80b11a47625460bd49320873e95ed2f9aee8d
leesua <s2332@e-mirim.hs.kr>
2024-11-29 10:20:30 +0900

    비밀번호 passwordField로 변경, db 연결

---
+++ src/main/java/com/example/webremind/DBConnection.java
@@ -0,0 +1,20 @@
+package com.example.webremind;
+import java.sql.*;
+class DBConnectionEx {
+    public static Connection getConnection() {
+        Connection conn = null;
+        String username="root"; // DB접속시 아이디
+        String password="111111"; // DB접속시 비밀번호
+        String url = "jdbc:mysql://localhost:3306/webreminddb";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, username,password);
+            System.out.println("DB 연결 완료");
+        } catch (ClassNotFoundException e) { //예외 처리
+            System.out.println("드라이버 오류");
+        } catch (SQLException e) {
+            System.out.println("데이터베이스 접속 정보 오류");
+        }
+        return conn;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.webremind {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.webremind to javafx.fxml;
