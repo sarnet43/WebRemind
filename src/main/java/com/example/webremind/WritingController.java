6ddf540e703dd5ab7b079b924fb352f1eba582f0
lmslh <s2330@e-mirim.hs.kr>
2024-11-29 10:25:53 +0900

    저장, 뒤로 가기 버튼 구현 작동 잘됨.

---
+++ src/main/java/com/example/webremind/WritingController.java
@@ -0,0 +1,56 @@
+package com.example.webremind;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WritingController {
+
+    @FXML
+    private javafx.scene.control.Button btn_save;          //홈 버튼 id
+
+    @FXML
+    private javafx.scene.control.Button btn_back;          //마이페이지 버튼 id
+
+    @FXML
+    private void onsave_wrButtonClick() {            //저장 버튼을 클릭 시 호출되는 메서드
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Alarm.fxml"));
+            Parent alarmScreen = loader.load();            // FXML 파일을 로드하여 Parent 변환
+
+            // 현재 Stage 가져오기
+            Stage stage = (Stage) btn_save.getScene().getWindow();
+
+            // 저장 화면으로 Scene 교체
+            Scene alarmScene = new Scene(alarmScreen);
+            stage.setScene(alarmScene);
+            // 화면을 새로운 Scene으로 생성하고 Stage에 설정
+        } catch (IOException e) {                           // IOException 발생 시 예외 처리
+            e.printStackTrace();                            // 에러 메시지를 출력
+        }
+    }
+
+    @FXML
+    private void onback_wrButtonClick() {            //뒤로 가기 버튼을 클릭 시 호출되는 메서드
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Community.fxml"));
+            Parent alarmScreen = loader.load();            // FXML 파일을 로드하여 Parent 변환
+
+            // 현재 Stage 가져오기
+            Stage stage = (Stage) btn_back.getScene().getWindow();
+
+
+            Scene alarmScene = new Scene(alarmScreen);
+            stage.setScene(alarmScene);
+            // 화면을 새로운 Scene으로 생성하고 Stage에 설정
+        } catch (IOException e) {                           // IOException 발생 시 예외 처리
+            e.printStackTrace();                            // 에러 메시지를 출력
+        }
+    }
+}
