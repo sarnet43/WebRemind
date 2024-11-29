06bd6bfdbf2120b91c21e5f3b5b12b9778776127
lmslh <s2330@e-mirim.hs.kr>
2024-11-29 10:29:17 +0900

    + 버튼을 누를 시 Writing.fxml 파일로 넘어가도록 구현.

--- src/main/java/com/example/webremind/CommunityController.java
+++ src/main/java/com/example/webremind/CommunityController.java
@@ -26,7 +26,7 @@ public class CommunityController {
     private void onalarm_comButtonClick() {            //알림 버튼을 클릭 시 호출되는 메서드
         try {
             // 알림 화면 로드
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/alarm.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Alarm.fxml"));
             Parent alarmScreen = loader.load();            // FXML 파일을 로드하여 Parent 변환
 
             // 현재 Stage 가져오기
@@ -87,7 +87,7 @@ public class CommunityController {
             Parent writingScreen = loader.load();            // FXML 파일을 로드하여 Parent 변환
 
             // 현재 Stage 가져오기
-            Stage stage = (Stage) btn_home.getScene().getWindow();      //글쓰기 버튼을 누를 시 새로운 스테이지 생성
+            Stage stage = (Stage) btn_add.getScene().getWindow();      //글쓰기 버튼을 누를 시 새로운 스테이지 생성
 
             // 글쓰기 화면으로 Scene 교체
             Scene writingScene = new Scene(writingScreen);
