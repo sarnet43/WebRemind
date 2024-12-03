834659d21e5c5ff3cec74e004a5d11cfa0b77c4b
lmslh <s2330@e-mirim.hs.kr>
2024-12-03 19:05:08 +0900

    desktop 추가

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.webremind {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens com.example.webremind to javafx.fxml;
