96c270113d6b9ac3780d5002de0f1e207350390e
lmslh <s2330@e-mirim.hs.kr>
2024-12-03 19:05:29 +0900

    기타

--- src/main/java/com/example/webremind/SingupController.java
+++ src/main/java/com/example/webremind/SingupController.java
@@ -14,19 +14,17 @@ public class SingupController {
     private javafx.scene.control.Button btn_next;          //다음 버튼 id
 
     @FXML
-    private void onnextButtonClick() {            //다음 버튼을 클릭 시 호출되는 메서드
+    private void onnextButtonClick() {
         try {
-            // 커뮤니티 화면 로드
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Community.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
             Parent communityScreen = loader.load();            // FXML 파일을 로드하여 Parent 변환
 
             // 현재 Stage 가져오기
             Stage stage = (Stage) btn_next.getScene().getWindow();      //다음 버튼을 누를 시 새로운 스테이지 생성
 
-            // 커뮤니티 화면으로 Scene 교체
             Scene communityScene = new Scene(communityScreen);
             stage.setScene(communityScene);
-            // 커뮤니티 화면을 새로운 Scene으로 생성하고 Stage에 설정
+
         } catch (IOException e) {                           // IOException 발생 시 예외 처리
             e.printStackTrace();                            // 에러 메시지를 출력
         }
