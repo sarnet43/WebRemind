58d177b4b4abbed2e89236b284f430f9d8474598
lmslh <s2330@e-mirim.hs.kr>
2024-12-03 21:50:21 +0900

    기타

--- src/main/java/com/example/webremind/DBConnection.java
+++ src/main/java/com/example/webremind/DBConnection.java
@@ -1,14 +1,16 @@
 package com.example.webremind;
+
 import java.sql.*;
+
 class DBConnectionEx {
     public static Connection getConnection() {
         Connection conn = null;
-        String username="root"; // DB접속시 아이디
-        String password="111111"; // DB접속시 비밀번호
+        String username = "root"; // DB접속시 아이디
+        String password = "111111"; // DB접속시 비밀번호
         String url = "jdbc:mysql://localhost:3306/webreminddb";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, username,password);
+            conn = DriverManager.getConnection(url, username, password);
             System.out.println("DB 연결 완료");
         } catch (ClassNotFoundException e) { //예외 처리
             System.out.println("드라이버 오류");
