56a30c098c018f54f7f0805c7330e3e808fb1bd8
lmslh <s2330@e-mirim.hs.kr>
2024-12-03 21:51:50 +0900

    길이 조절. 10자 이상일 시 글자 추가 불가능

--- src/main/java/com/example/webremind/LoingController.java
+++ src/main/java/com/example/webremind/LoingController.java
@@ -4,11 +4,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoingController {
+
+    @FXML
+    private TextField userid; // 아이디
+
+    @FXML
+    private PasswordField password; // 유저 닉네임
 
     @FXML
     private javafx.scene.control.Button btn_sing;           //회원가입 버튼 id
@@ -17,7 +26,19 @@ public class LoingController {
     private javafx.scene.control.Button btn_login;          //로그인 버튼 id
 
     @FXML
+    private void initialize() {
+        //id 길이 조절
+        userid.textProperty().addListener((observable, oldValue, newValue) -> {
+            // 텍스트 길이가 10자 이상이면, 글자를 더 이상 추가할 수 없도록 함
+            if (newValue.length() > 10) {
+                userid.setText(oldValue); // 이전 값으로 되돌림
+            }
+        });
+    }
+
+    @FXML
     private void onSignupButtonClick() {            //회원가입 버튼을 클릭 시 호출되는 메서드
+
         try {
             // 회원가입 화면 로드
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/SingUp.fxml"));
@@ -37,6 +58,16 @@ public class LoingController {
 
     @FXML
     private void onLoginButtonClick(){             //로그인 버튼을 클릭 시 호출되는 메서드
+
+        String user_id = userid.getText().trim(); // 닉네임을 가져옴
+        String uswer_password = password.getText().trim(); // 비밀번호를 가져옴
+
+        // 제목과 내용이 모두 비어있을 경우 경고 표시
+        if (user_id.isEmpty() || uswer_password.isEmpty()) {
+            showAlert("오류", "모두 입력하였는 지 확인하세요.");
+            return;
+        }
+
         try {
             // 로그인 화면 로드
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Community.fxml"));
@@ -53,4 +84,14 @@ public class LoingController {
             e.printStackTrace();                    // 에러 메시지를 출력
         }
     }
+
+    // 경고창 표시 메서드
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 }
